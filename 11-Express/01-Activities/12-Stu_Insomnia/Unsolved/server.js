27033c76894a73b17da5719ec9a6348adf7e65fc
Gerry Paul <gpaul12@nc.rr.com>
2023-04-03 19:24:57 -0400

    12

--- 11-Express/01-Activities/12-Stu_Insomnia/Unsolved/server.js
+++ 11-Express/01-Activities/12-Stu_Insomnia/Unsolved/server.js
@@ -1,14 +1,21 @@
-const express = require('express');
+const express = require("express");
 // TODO: Require the json file located in `/db`
+const repos = require("./db/repos.json");
 
 // TODO: Create an `app` variable set to the value of `express()`
+var app = express();
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send(
     'Use the API endpoint at <a href="http://localhost:3001/api">localhost:3001/api</a>'
   );
 });
 
 // TODO: Create a GET route for `/api` that will return the content of our json file
+app.get("/api", (req, res) => res.json(repos));
 
+app.listen(PORT, () => {
+  console.log("App listening");
+});
 // TODO: Have the app listen on port 3001
+const PORT = 3001;
