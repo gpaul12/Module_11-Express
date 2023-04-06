553e62eeb7b2329f61406005dff1ff31dc0caa61
Gerry Paul <gpaul12@nc.rr.com>
2023-04-06 16:54:23 -0400

    28

--- 11-Express/01-Activities/28-Stu_Mini-Project/Develop/routes/diagnostics.js
+++ 11-Express/01-Activities/28-Stu_Mini-Project/Develop/routes/diagnostics.js
@@ -1,15 +1,39 @@
-const diagnostics = require('express').Router();
-const { v4: uuidv4 } = require('uuid');
-const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+const diagnostics = require("express").Router();
+const { v4: uuidv4 } = require("uuid");
+const { readAndAppend, readFromFile } = require("../helpers/fsUtils");
 
 // GET Route for retrieving diagnostic information
-diagnostics.get('/', (req, res) => {
+diagnostics.get("/", (req, res) => {
   // TODO: Logic for sending all the content of db/diagnostics.json
+  readFromFile("./db/diagnostics.json").then((data) =>
+    res.json(JSON.parse(data))
+  );
 });
 
 // POST Route for a error logging
-diagnostics.post('/', (req, res) => {
+diagnostics.post("/", (req, res) => {
   // TODO: Logic for appending data to the db/diagnostics.json file
+  const { time, error_id, errors } = req.body;
+
+  if (email && feedbackType && feedback) {
+    const newFeedback = {
+      email,
+      feedbackType,
+      feedback,
+      feedback_id: uuidv4(),
+    };
+
+    readAndAppend(newFeedback, "./db/diagnostics.json");
+
+    const response = {
+      status: "success",
+      body: newFeedback,
+    };
+
+    res.json(response);
+  } else {
+    res.json("Error in posting feedback");
+  }
 });
 
 module.exports = diagnostics;
